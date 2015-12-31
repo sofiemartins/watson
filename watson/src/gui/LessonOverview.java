709139b4dac73e9bc082e3403c22a7fffd5d0793
Sofie Martins <martinss@physik.hu-berlin.de>
2015-12-31 05:41:21 +0100

    Added the skeletons for buttons on the lesson overview

--- watson/src/gui/LessonOverview.java
+++ watson/src/gui/LessonOverview.java
@@ -11,6 +11,10 @@
 
 package gui;
 
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class LessonOverview {
 	
 	public LessonOverview(){
@@ -20,5 +24,39 @@ public class LessonOverview {
 	private void load(){}
 	
 	private void save(){}
+	
+	public JButton getNewButton(){
+		JButton button = new JButton("New"); // TODO: add icon
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				new NewLessonDialog();
+			}
+		});
+		return button;
+	}
+	
+	public JButton getRemoveButton(){
+		JButton button = new JButton("Remove");
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				//remove lesson
+			}
+		});
+		return button;
+	}
+	
+	public JButton getEditButton(){
+		JButton button = new JButton("Edit");
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				
+			}
+		})
+	}
+	
+	
 
 }
