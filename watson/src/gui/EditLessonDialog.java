25115f1d86d509d9e613ac4331e247c287d61fbf
Sofie Martins <martinss@physik.hu-berlin.de>
2015-12-31 19:52:19 +0100

    added icons and action listeners to buttons

--- watson/src/gui/EditLessonDialog.java
+++ watson/src/gui/EditLessonDialog.java
@@ -3,9 +3,12 @@ package gui;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
 import static java.awt.BorderLayout.*;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 import util.Lesson;
 
@@ -34,13 +37,62 @@ public class EditLessonDialog extends JFrame{
 	private JPanel getTopPanel(){}
 	
 	private JButton nextButton(){
+		JButton button = new JButton();
+		button.setIcon(new ImageIcon(getClass().getResource("next.png")));
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				//next card
+			}
+		});
+		return button;
 	}
-	private JButton backButton(){}
-	private JButton otherSideButton(){}
+	private JButton backButton(){
+		JButton button = new JButton();
+		button.setIcon(new ImageIcon(getClass().getResource("back.png")));
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				//one card back
+			}
+		});
+		return button;
+	}
+	private JButton otherSideButton(){
+		JButton button = new JButton();
+		button.setIcon(new ImageIcon(getClass().getResource("otherside.png")));
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				//show other side
+			}
+		});
+		return button;
+	}
 	
 	private JPanel getBottomPanel(){}
 	
-	private JButton deleteButton(){}
-	private JButton addButton(){}
+	private JButton deleteButton(){
+		JButton button = new JButton();
+		button.setIcon(new ImageIcon(getClass().getResource("remove.png")));
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				// delete
+			}
+		});
+		return button;
+	}
+	private JButton addButton(){
+		JButton button = new JButton();
+		button.setIcon(new ImageIcon(getClass().getResource("add.png")));
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				//add card
+			}
+		});
+		return button;
+	}
 
 }
