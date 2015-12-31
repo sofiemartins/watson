8f5231b99e6fa0c62bf81bca205a135cc73a4976
Sofie Martins <martinss@physik.hu-berlin.de>
2015-12-31 04:08:34 +0100

    Created a FileManager class for dealing with files

--- watson/src/gui/LessonOverview.java
+++ watson/src/gui/LessonOverview.java
@@ -1,5 +1,24 @@
+/**
+ *  This shows up after starting the program and is some kind of "main menu".
+ *  What users can do here:
+ *  <ol>
+ *    <li> add lessons
+ *    <li> remove lessons
+ *    <li> edit lessons
+ *    <li> start practicing a certain lesson
+ *  </ol>
+ */
+
 package gui;
 
 public class LessonOverview {
+	
+	public LessonOverview(){
+		
+	}
+	
+	private void load(){}
+	
+	private void save(){}
 
 }
