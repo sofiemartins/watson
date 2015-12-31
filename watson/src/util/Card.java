fa25c2ab73725a2cb811abd41d430f4ee272b1bf
Sofie Martins <martinss@physik.hu-berlin.de>
2015-12-31 04:17:48 +0100

    Made Card and Lesson serializable

--- watson/src/util/Card.java
+++ watson/src/util/Card.java
@@ -1,5 +1,9 @@
 package util;
 
-public class Card {
+import java.io.Serializable;
+
+public class Card implements Serializable{
+	
+	public static final long serialVersionUID = 3658394727116385564L; 
 
 }
--- watson/src/util/Lesson.java
+++ watson/src/util/Lesson.java
@@ -1,5 +1,9 @@
 package util;
 
-public class Lesson {
+import java.io.Serializable;
+
+public class Lesson implements Serializable{
+	
+	public static final long serialVersionUID = 996887968500987365L;
 
 }
