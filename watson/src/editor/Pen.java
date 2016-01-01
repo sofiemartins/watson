ba379de52c7350d5f6331e388149706253a92cd2
Sofie Martins <martinss@physik.hu-berlin.de>
2016-01-01 23:55:36 +0100

    Editor works, I'm going to adjust the details later

--- watson/src/editor/Pen.java
+++ watson/src/editor/Pen.java
@@ -5,20 +5,20 @@ import static editor.PenMode.NONE;
 import java.awt.Color;
 
 public class Pen {
-	
+	// TODO: Size slider
 	private int size = 2; //px
 	private Color color = Color.black;
 	private PenMode mode = NONE;
 	
 	private static final Color markerColor = new Color(255,240,0,100);
-	private static final int markerSize = 6; //px
+	private static final int markerSize = 25; //px
 	public static final int BIG = 4;
 	public static final int MEDIUM = 3;
 	public static final int SMALL = 2;
 	
 	public static final Pen MARKER = new Pen(markerSize, markerColor, NONE);
 	public static final Pen PEN = new Pen(MEDIUM, Color.black, NONE);
-	public static final Pen ERASER = new Pen(4, new Color(0,0,0,0), NONE);
+	public static final Pen ERASER = new Pen(4, Color.white, NONE);//TODO: Make a real eraser, not just a white pen
 			
 	public Pen(){}
 	
