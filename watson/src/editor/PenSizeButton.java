3aaa16810d9aaedc6f875c38ced8d05ef99081fd
Sofie Martins <martinss@physik.hu-berlin.de>
2016-01-14 23:11:37 +0100

    Cleaned up pen size button

--- watson/src/editor/PenSizeButton.java
+++ watson/src/editor/PenSizeButton.java
@@ -8,6 +8,7 @@ import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import static java.awt.event.ActionEvent.ACTION_PERFORMED;
 
 public class PenSizeButton extends JToggleButton implements ActionListener{
 	
@@ -25,10 +26,25 @@ public class PenSizeButton extends JToggleButton implements ActionListener{
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		drawSymbol(g);
+	}
+	
+	private void drawSymbol(Graphics g){
 		g.setColor(Color.black);
-		int sizeInPx = Pen.getSizeInPx(penSize, Editor.currentPen.getType());
-		g.fillOval((int)(getWidth()*0.5-sizeInPx*0.5), (int)(getHeight()*0.5-sizeInPx*0.5),
-				sizeInPx, sizeInPx);
+		g.fillOval((int)(getWidth()*0.5-getSizeInPx()*0.5), (int)(getHeight()*0.5-getSizeInPx()*0.5),
+				getSizeInPx(), getSizeInPx());
+	}
+	
+	private int getSizeInPx(){
+		return Pen.getSizeInPx(penSize, getCurrentPen().getType());
+	}
+	
+	private Pen getCurrentPen(){
+		return Editor.currentPen;
+	}
+	
+	private Pen getNewPen(){
+		return new Pen(penSize, getCurrentPen().getColor(), getCurrentPen().getMode(), getCurrentPen().getType());
 	}
 	
 	public void addActionListener(ActionListener al){
@@ -36,12 +52,11 @@ public class PenSizeButton extends JToggleButton implements ActionListener{
 	}
 	
 	public void actionPerformed(ActionEvent e){
-		Pen currentPen = Editor.currentPen;
-		Pen newPen = new Pen(penSize, currentPen.getColor(), currentPen.getMode(), currentPen.getType());
-		actionListener.actionPerformed(new ToolbarEvent(this,
-				ActionEvent.ACTION_PERFORMED,
-				"size was changed",
-				newPen));
+		actionListener.actionPerformed(triggeredEvent());
 		setSelected(true);
+	}
+	
+	private ToolbarEvent triggeredEvent(){
+		return new ToolbarEvent(this, ACTION_PERFORMED, "size was changed", getNewPen());
 	}
 }
