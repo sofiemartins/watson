3802fddd1d33169f0f3be64eab2995ce9aa5553d
Sofie Martins <martinss@physik.hu-berlin.de>
2016-01-14 22:36:35 +0100

    Cleaned up color button code

--- watson/src/editor/ColorButton.java
+++ watson/src/editor/ColorButton.java
@@ -8,6 +8,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import static java.awt.event.ActionEvent.ACTION_PERFORMED;
 
 public class ColorButton extends JToggleButton implements ActionListener{
 	
@@ -24,8 +25,20 @@ public class ColorButton extends JToggleButton implements ActionListener{
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		paintSymbol(g);
+	}
+	
+	private void paintSymbol(Graphics g){
+		fillRectangle(g);
+		drawOutline(g);
+	}
+	
+	private void fillRectangle(Graphics g){
 		g.setColor(buttonColor);
 		g.fillRect(5, 5, getWidth()-10, getHeight()-10);
+	}
+	
+	private void drawOutline(Graphics g){
 		g.setColor(Color.black);
 		g.drawRect(5, 5, getWidth()-10, getHeight()-10);
 	}
@@ -39,12 +52,20 @@ public class ColorButton extends JToggleButton implements ActionListener{
 	}
 	
 	public void actionPerformed(ActionEvent e){
-		Pen currentPen = Editor.currentPen;
-		Pen newPen = new Pen(currentPen.getSize(), buttonColor, currentPen.getMode(), currentPen.getType());
-		actionListener.actionPerformed(new ToolbarEvent(this,
-				ActionEvent.ACTION_PERFORMED,
-				"color button was pressed",
-				newPen));
+		actionListener.actionPerformed(triggeredEvent());
 		setSelected(true);
 	}
+	
+	private Pen getCurrentPen(){
+		return Editor.currentPen;
+	}
+	
+	private Pen getNewPen(){
+		return new Pen(getCurrentPen().getSize(), buttonColor, getCurrentPen().getMode(), getCurrentPen().getType());
+	}
+	
+	private ToolbarEvent triggeredEvent(){
+		return new ToolbarEvent(this, ACTION_PERFORMED, "Color button was pressed.", getNewPen());
+	}
+	
 }
