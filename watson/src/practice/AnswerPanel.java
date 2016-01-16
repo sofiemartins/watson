2255211ba748cad73bddad6ad73f8878a8bd755f
Sofie Martins <martinss@physik.hu-berlin.de>
2016-01-16 22:01:47 +0100

    Deleted AnswerButton because it has been replaced by RightAnswerButton and WrongAnswerButton

--- watson/src/practice/AnswerPanel.java
+++ watson/src/practice/AnswerPanel.java
@@ -5,7 +5,6 @@ import java.awt.KeyEventDispatcher;
 import java.awt.event.KeyEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.KeyboardFocusManager;
 
