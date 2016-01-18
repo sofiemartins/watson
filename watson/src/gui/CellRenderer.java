49a986cfc5ad64ad1bae322ea6087ac60110ce8f
Sofie Martins <martinss@physik.hu-berlin.de>
2016-01-18 01:16:50 +0100

    Added styling for selected cells

--- watson/src/gui/CellRenderer.java
+++ watson/src/gui/CellRenderer.java
@@ -11,7 +11,9 @@ public class CellRenderer extends DefaultListCellRenderer{
 	
 	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus){
 		Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-		if(index%2 == 0){
+		if(isSelected){
+			component.setBackground(new Color(230, 235, 230));
+		}else if(index%2==0){
 			component.setBackground(new Color(240, 240, 240));
 		}else{
 			component.setBackground(Color.white);
