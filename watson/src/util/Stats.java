5fb77c5154b5b5a854cda202a3931ad00e1838c1
Sofie Martins <martinss@physik.hu-berlin.de>
2016-01-18 02:04:30 +0100

    Added stats object to store the statssets

--- watson/src/util/Stats.java
+++ watson/src/util/Stats.java
@@ -1,6 +1,19 @@
 package util;
 
-public class Stats {
-	
+import java.io.Serializable;
+import java.util.LinkedList;
 
+public class Stats implements Serializable{
+	
+	public static final long serialVersionUID = 8857747768575112321L;
+	
+	private LinkedList<StatsSet> statsList = new LinkedList<StatsSet>();
+	
+	public void addStats(StatsSet stats){
+		statsList.add(stats);
+	}
+	
+	public LinkedList<StatsSet> getStats(){
+		return statsList;
+	}
 }
