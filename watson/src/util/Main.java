5277a908e44fda64250f742af5e07b6978cd6e28
Sofie Martins <martinss@physik.hu-berlin.de>
2016-02-15 23:02:40 +0100

    invokeLater prevents ugly synth error

--- watson/src/util/Main.java
+++ watson/src/util/Main.java
@@ -1,10 +1,17 @@
 package util;
 
+import javax.swing.SwingUtilities;
+
 import gui.LessonOverview;
 
 public class Main {
 	
 	public static void main(String[] args){
-		new LessonOverview();
+		SwingUtilities.invokeLater(new Runnable(){
+			@Override
+			public void run(){
+				new LessonOverview();
+			}
+		});
 	}
 }
